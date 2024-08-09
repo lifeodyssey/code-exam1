b502b8798fb4cb180e5c9058aea24eeae72f5e9d
lifeodyssey <zhouthepassion@outlook.com>
2024-08-09 10:58:35 +0800

    refactor(sql): use postgres auto increment for the id, remove sequence

--- book/src/main/resources/db/migration/V1__Create_books_table_and_insert_initial_data.sql
+++ book/src/main/resources/db/migration/V1__Create_books_table_and_insert_initial_data.sql
@@ -1,19 +1,13 @@
 CREATE TABLE books (
-                       id integer PRIMARY KEY,
-                       title varchar(100),
-                       author varchar(100),
-                       publisher varchar(100),
-                       price integer
+                       id SERIAL PRIMARY KEY ,
+                       title VARCHAR(100),
+                       author VARCHAR(100),
+                       publisher VARCHAR(100),
+                       price INTEGER
 );
 
-CREATE SEQUENCE IF NOT EXISTS BOOK_ID_SEQ
-    INCREMENT BY 1
-    MAXVALUE 9999999999
-    MINVALUE 1
-    START WITH 1
-;
-
-INSERT INTO books (id, title, author, publisher, price) VALUES (nextval('BOOK_ID_SEQ'), 'テスト駆動開発', 'Kent Beck', 'オーム社', 3080);
-INSERT INTO books (id, title, author, publisher, price) VALUES (nextval('BOOK_ID_SEQ'), 'アジャイルサムライ', 'Jonathan Rasmusson', 'オーム社', 2860);
-INSERT INTO books (id, title, author, publisher, price) VALUES (nextval('BOOK_ID_SEQ'), 'エクストリームプログラミング', 'Kent Beck', 'オーム社', 2420);
-INSERT INTO books (id, title, author, publisher, price) VALUES (nextval('BOOK_ID_SEQ'), 'Clean Agile', 'Robert C. Martin', 'ドワンゴ', 2640);
+INSERT INTO books (title, author, publisher, price) VALUES
+                                                        ('テスト駆動開発', 'Kent Beck', 'オーム社', 3080),
+                                                        ('アジャイルサムライ', 'Jonathan Rasmusson', 'オーム社', 2860),
+                                                        ('エクストリームプログラミング', 'Kent Beck', 'オーム社', 2420),
+                                                        ('Clean Agile', 'Robert C. Martin', 'ドワンゴ', 2640);
