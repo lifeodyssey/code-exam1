d756169a173f80b3798de3f3c3c679d68eaa1798
lifeodyssey <zhouthepassion@outlook.com>
2024-08-09 18:24:26 +0800

    fix(create book api): use @transactional to rolled back after test complete

--- book/src/test/java/org/aimodel/book/repository/BookRepositoryTest.java
+++ book/src/test/java/org/aimodel/book/repository/BookRepositoryTest.java
@@ -1,53 +1,57 @@
 package org.aimodel.book.repository;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
+import jakarta.transaction.Transactional;
 import org.aimodel.book.ApplicationTestBase;
 import org.aimodel.book.repository.entity.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
+@Transactional
 class BookRepositoryTest extends ApplicationTestBase {
 
-  @Autowired
-  private BookRepository bookRepository;
+    @Autowired
+    private BookRepository bookRepository;
 
-  @Test
-  void shouldFindAllBooks() {
-    List<Book> expectedBooks =
-        List.of(
-            new Book(1, "テスト駆動開発", "Kent Beck", "オーム社", 3080),
-            new Book(2, "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860),
-            new Book(3, "エクストリームプログラミング", "Kent Beck", "オーム社", 2420),
-            new Book(4, "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640));
-    List<Book> books = bookRepository.findAll();
-    assertThat(books).isEqualTo(expectedBooks);
-  }
 
-  @Test
-  void shouldSaveNewBook() {
-    // Given
-    Book newBook = Book.builder()
-            .title("新しい本")
-            .author("新しい著者")
-            .publisher("新しい出版社")
-            .price(1000)
-            .build();
-    // When
-    Book savedBook = bookRepository.save(newBook);
+    @Test
+    void shouldFindAllBooks() {
+        List<Book> expectedBooks =
+                List.of(
+                        new Book(1, "テスト駆動開発", "Kent Beck", "オーム社", 3080),
+                        new Book(2, "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860),
+                        new Book(3, "エクストリームプログラミング", "Kent Beck", "オーム社", 2420),
+                        new Book(4, "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640));
+        List<Book> books = bookRepository.findAll();
+        assertThat(books).isEqualTo(expectedBooks);
+    }
 
-    // Then
-    assertThat(savedBook.getId()).isNotNull();
-    assertThat(savedBook.getId()).isEqualTo(5);
-    assertThat(savedBook.getTitle()).isEqualTo("新しい本");
-    assertThat(savedBook.getAuthor()).isEqualTo("新しい著者");
-    assertThat(savedBook.getPublisher()).isEqualTo("新しい出版社");
-    assertThat(savedBook.getPrice()).isEqualTo(1000);
+    @Test
+    void shouldSaveNewBook() {
+        // Given
+        Book newBook = Book.builder()
+                .title("新しい本")
+                .author("新しい著者")
+                .publisher("新しい出版社")
+                .price(1000)
+                .build();
+        // When
+        Book savedBook = bookRepository.save(newBook);
 
-    Book foundBook = bookRepository.findById(savedBook.getId()).orElse(null);
-    assertThat(foundBook).usingRecursiveComparison().isEqualTo(savedBook);
-  }
+        // Then
+        assertThat(savedBook.getId()).isNotNull();
+        assertThat(savedBook.getId()).isEqualTo(5);
+        assertThat(savedBook.getTitle()).isEqualTo("新しい本");
+        assertThat(savedBook.getAuthor()).isEqualTo("新しい著者");
+        assertThat(savedBook.getPublisher()).isEqualTo("新しい出版社");
+        assertThat(savedBook.getPrice()).isEqualTo(1000);
+
+        Book foundBook = bookRepository.findById(savedBook.getId()).orElse(null);
+        assertThat(foundBook).usingRecursiveComparison().isEqualTo(savedBook);
+    }
 }
--- book/src/test/java/org/aimodel/book/service/BookServiceImplTest.java
+++ book/src/test/java/org/aimodel/book/service/BookServiceImplTest.java
@@ -1,11 +1,5 @@
 package org.aimodel.book.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.List;
 import org.aimodel.book.controller.dto.BookDto;
 import org.aimodel.book.repository.BookRepository;
 import org.aimodel.book.repository.entity.Book;
@@ -15,77 +9,83 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
 class BookServiceImplTest {
-  @Mock
-  private BookRepository bookRepository;
-  @Mock
-  private BookMapper bookMapper;
+    @Mock
+    private BookRepository bookRepository;
+    @Mock
+    private BookMapper bookMapper;
 
-  @InjectMocks
-  private BookServiceImpl bookService;
+    @InjectMocks
+    private BookServiceImpl bookService;
 
-  @BeforeEach
-  void setUp() {
-    MockitoAnnotations.openMocks(this);
-  }
-  @Test
-  void getAllBooksShouldReturnAllBooksAsDto() {
-    // Given
-    List<Book> books =
-        List.of(
-            new Book(1, "テスト駆動開発", "Kent Beck", "オーム社", 3080),
-            new Book(2, "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860));
-    List<BookDto> expectedBookDtos =
-        List.of(
-            new BookDto(1, "テスト駆動開発", "Kent Beck", "オーム社", 3080),
-            new BookDto(2, "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860));
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
-    when(bookRepository.findAll()).thenReturn(books);
-    when(bookMapper.toDtoList(books)).thenReturn(expectedBookDtos);
+    @Test
+    void getAllBooksShouldReturnAllBooksAsDto() {
+        // Given
+        List<Book> books =
+                List.of(
+                        new Book(1, "テスト駆動開発", "Kent Beck", "オーム社", 3080),
+                        new Book(2, "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860));
+        List<BookDto> expectedBookDtos =
+                List.of(
+                        new BookDto(1, "テスト駆動開発", "Kent Beck", "オーム社", 3080),
+                        new BookDto(2, "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860));
 
-    // When
-    List<BookDto> actualBookDtos = bookService.getAllBooks();
+        when(bookRepository.findAll()).thenReturn(books);
+        when(bookMapper.toDtoList(books)).thenReturn(expectedBookDtos);
 
-    // Then
-    assertThat(actualBookDtos).isEqualTo(expectedBookDtos);
-    verify(bookRepository, times(1)).findAll();
-    verify(bookMapper, times(1)).toDtoList(books);
-  }
+        // When
+        List<BookDto> actualBookDtos = bookService.getAllBooks();
 
-  @Test
-  void getAllBooksShouldReturnEmptyListWhenNoBooks() {
-    // Given
-    when(bookRepository.findAll()).thenReturn(List.of());
-    when(bookMapper.toDtoList(List.of())).thenReturn(List.of());
+        // Then
+        assertThat(actualBookDtos).isEqualTo(expectedBookDtos);
+        verify(bookRepository, times(1)).findAll();
+        verify(bookMapper, times(1)).toDtoList(books);
+    }
 
-    // When
-    List<BookDto> actualBookDtos = bookService.getAllBooks();
+    @Test
+    void getAllBooksShouldReturnEmptyListWhenNoBooks() {
+        // Given
+        when(bookRepository.findAll()).thenReturn(List.of());
+        when(bookMapper.toDtoList(List.of())).thenReturn(List.of());
 
-    // Then
-    assertThat(actualBookDtos).isEmpty();
-    verify(bookRepository, times(1)).findAll();
-    verify(bookMapper, times(1)).toDtoList(List.of());
-  }
+        // When
+        List<BookDto> actualBookDtos = bookService.getAllBooks();
 
-  @Test
-  void createBookShouldSaveAndReturnBookAsDto() {
-    // Given
-    BookDto inputBookDto = new BookDto(null, "新しい本", "新しい著者", "新しい出版社", 1000);
-    Book inputBook = new Book(null, "新しい本", "新しい著者", "新しい出版社", 1000);
-    Book savedBook = new Book(1, "新しい本", "新しい著者", "新しい出版社", 1000);
-    BookDto expectedBookDto = new BookDto(1, "新しい本", "新しい著者", "新しい出版社", 1000);
+        // Then
+        assertThat(actualBookDtos).isEmpty();
+        verify(bookRepository, times(1)).findAll();
+        verify(bookMapper, times(1)).toDtoList(List.of());
+    }
 
-    when(bookMapper.toEntity(inputBookDto)).thenReturn(inputBook);
-    when(bookRepository.save(inputBook)).thenReturn(savedBook);
-    when(bookMapper.toDto(savedBook)).thenReturn(expectedBookDto);
+    @Test
+    void createBookShouldSaveAndReturnBookAsDto() {
+        // Given
+        BookDto inputBookDto = new BookDto(null, "新しい本", "新しい著者", "新しい出版社", 1000);
+        Book inputBook = new Book(null, "新しい本", "新しい著者", "新しい出版社", 1000);
+        Book savedBook = new Book(1, "新しい本", "新しい著者", "新しい出版社", 1000);
+        BookDto expectedBookDto = new BookDto(1, "新しい本", "新しい著者", "新しい出版社", 1000);
 
-    // When
-    BookDto actualBookDto = bookService.createBook(inputBookDto);
+        when(bookMapper.toEntity(inputBookDto)).thenReturn(inputBook);
+        when(bookRepository.save(inputBook)).thenReturn(savedBook);
+        when(bookMapper.toDto(savedBook)).thenReturn(expectedBookDto);
 
-    // Then
-    assertThat(actualBookDto).isEqualTo(expectedBookDto);
-    verify(bookMapper, times(1)).toEntity(inputBookDto);
-    verify(bookRepository, times(1)).save(inputBook);
-    verify(bookMapper, times(1)).toDto(savedBook);
-  }
+        // When
+        BookDto actualBookDto = bookService.createBook(inputBookDto);
+
+        // Then
+        assertThat(actualBookDto).isEqualTo(expectedBookDto);
+        verify(bookMapper, times(1)).toEntity(inputBookDto);
+        verify(bookRepository, times(1)).save(inputBook);
+        verify(bookMapper, times(1)).toDto(savedBook);
+    }
 }
