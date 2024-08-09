9699ad872ca31c39b24c78e16ca72da4ce111171
lifeodyssey <zhouthepassion@outlook.com>
2024-08-09 20:44:00 +0800

    style: format code

--- book/src/main/java/org/aimodel/book/service/BookServiceImpl.java
+++ book/src/main/java/org/aimodel/book/service/BookServiceImpl.java
@@ -1,33 +1,35 @@
 package org.aimodel.book.service;
 
-import java.util.List;
 import org.aimodel.book.controller.dto.BookDto;
 import org.aimodel.book.repository.BookRepository;
 import org.aimodel.book.repository.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookServiceImpl implements BookService {
-  private final BookRepository bookRepository;
-  private final BookMapper bookMapper;
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
 
-  @Autowired
-  public BookServiceImpl(BookRepository bookRepository, BookMapper bookMapper) {
-    this.bookRepository = bookRepository;
-    this.bookMapper = bookMapper;
-  }
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository, BookMapper bookMapper) {
+        this.bookRepository = bookRepository;
+        this.bookMapper = bookMapper;
+    }
 
-  @Override
-  public List<BookDto> getAllBooks() {
-    List<Book> books = bookRepository.findAll();
-    return bookMapper.toDtoList(books);
-  }
-  @Override
-  public BookDto createBook(BookDto bookDto) {
-    Book book = bookMapper.toEntity(bookDto);
-    Book savedBook = bookRepository.save(book);
-    return bookMapper.toDto(savedBook);
-  }
+    @Override
+    public List<BookDto> getAllBooks() {
+        List<Book> books = bookRepository.findAll();
+        return bookMapper.toDtoList(books);
+    }
+
+    @Override
+    public BookDto createBook(BookDto bookDto) {
+        Book book = bookMapper.toEntity(bookDto);
+        Book savedBook = bookRepository.save(book);
+        return bookMapper.toDto(savedBook);
+    }
 }
